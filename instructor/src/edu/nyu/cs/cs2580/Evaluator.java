fa62399ed1e8353cb111c2fa37af46007654f231
Samit <samit.patel@nyu.edu>
2013-02-23 20:00:09 -0500

    Implemented F0.50 @ 1 5 10

--- instructor/src/edu/nyu/cs/cs2580/Evaluator.java
+++ instructor/src/edu/nyu/cs/cs2580/Evaluator.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.Vector;
@@ -111,7 +112,7 @@ class Evaluator {
 				String line = null;
 				Vector<String> results = new Vector<String>();
 				String query = null;
-				
+
 				boolean gotQuery = false;
 				while ((line = reader.readLine()) != null){
 					if(!gotQuery){
@@ -130,6 +131,10 @@ class Evaluator {
 				Double recall_1 = calculateRecall(results, 1, relevance_judgments);
 				Double recall_5 = calculateRecall(results, 5, relevance_judgments);
 				Double recall_10 = calculateRecall(results, 10, relevance_judgments);
+				
+				Double f0_50_1 = calculateF(results, 1, relevance_judgments, 0.5);
+				Double f0_50_5 = calculateF(results, 5, relevance_judgments, 0.5);
+				Double f0_50_10 = calculateF(results, 10, relevance_judgments, 0.5);
 
 				String output = query+"\t" + 
 						Double.toString(precision_1) + "\t" + 
@@ -137,7 +142,10 @@ class Evaluator {
 						Double.toString(precision_10) + "\t" + 
 						Double.toString(recall_1) + "\t" + 
 						Double.toString(recall_5) + "\t" + 
-						Double.toString(recall_10) + "\n";
+						Double.toString(recall_10) + "\t" + 
+						Double.toString(f0_50_1) + "\t" + 
+						Double.toString(f0_50_5) + "\t" + 
+						Double.toString(f0_50_10) + "\n";
 
 
 				System.out.println(output);
@@ -162,12 +170,13 @@ class Evaluator {
 
 		if(k == 0 || results.size() == 0) return 0.0;
 		
-		double RR = 0.0;
-
-		String line = null;
+		if(PrecisionRecord.containsKey(k)) return PrecisionRecord.get(k);
+		
+		double precision = 0.0;
 
 		try{
-
+			double RR = 0.0;
+			String line = null;
 			for(int i=0; i<k; i++){
 				line = results.get(i);
 				Scanner s = new Scanner(line).useDelimiter("\t");
@@ -183,40 +192,50 @@ class Evaluator {
 					RR += qr.get(did);					
 				}
 			}
+			
+			precision = RR/k;
+			
+			PrecisionRecord.put(k, RR);
+			
 		}catch (Exception e){
 			System.err.println("Error:" + e.getMessage());
 		}
-		return (RR/k);
+		
+		return precision;
 	}
 
 
 	/**
-	 * Calculates the Recall
+	 * Calculates the Recall at k
 	 * */
 	private static Double calculateRecall(Vector<String> results, int k, 
 			HashMap < String , HashMap < Integer , Double > > relevance_judgments){
 
 		if(k == 0 || results.size() == 0) return 0.0;
 
-		double RR = 0.0;
-		double R = 0.0;
-		
-		String line = results.get(0);
+		if(RecallRecord.containsKey(k)) return RecallRecord.get(k);
 
-		Scanner s = new Scanner(line).useDelimiter("\t");
-		String query = s.next();
-		s.close();
-
-		HashMap<Integer, Double> relevantJudgements = relevance_judgments.get(query);
-		Set<Integer> documents = relevantJudgements.keySet();
-		Iterator<Integer> iter = documents.iterator();
-
-		while(iter.hasNext()){
-			Integer did = iter.next();
-			R += relevantJudgements.get(did);
-		}
+		double recall = 0.0;
 
 		try{
+
+			double RR = 0.0;
+			double R = 0.0;
+			
+			String line = results.get(0);
+
+			Scanner s = new Scanner(line).useDelimiter("\t");
+			String query = s.next();
+			s.close();
+
+			HashMap<Integer, Double> relevantJudgements = relevance_judgments.get(query);
+			Set<Integer> documents = relevantJudgements.keySet();
+			Iterator<Integer> iter = documents.iterator();
+
+			while(iter.hasNext()){
+				Integer did = iter.next();
+				R += relevantJudgements.get(did);
+			}
 
 			for(int i=0; i<k; i++){
 				line = results.get(i);
@@ -225,19 +244,43 @@ class Evaluator {
 				int did = Integer.parseInt(s.next());
 				String title = s.next();
 				double rel = Double.parseDouble(s.next());
+				
 				if (relevance_judgments.containsKey(query) == false){
 					throw new IOException("query not found");
 				}
+				
 				HashMap < Integer , Double > qr = relevance_judgments.get(query);
 				if (qr.containsKey(did) != false){
 					RR += qr.get(did);					
 				}
-				
 			}
+			
+			recall = RR/R;
+			
+			RecallRecord.put(k, recall);
+			
 		}catch (Exception e){
 			System.err.println("Error:" + e.getMessage());
 		}
-		return (RR/R);
+		return recall;
 	}
 
+	/**
+	 * Calculates the Recall at k
+	 * */
+	private static Double calculateF(Vector<String> results, int k, 
+			HashMap < String , HashMap < Integer , Double > > relevance_judgments, Double alpha){
+		
+		Double precision = calculatePrecision(results, k, relevance_judgments);
+		Double recall = calculateRecall(results, k, relevance_judgments);
+		
+		Double temp = (alpha*(1/precision) + (1-alpha)*(1/recall));
+		
+		return Math.pow(temp,-1);
+	}
+	
+	//Keeps the records of already calculated metrics
+	private static Map<Integer, Double> PrecisionRecord = new HashMap<Integer, Double>();
+	private static Map<Integer, Double> RecallRecord = new HashMap<Integer, Double>();
+	
 }
