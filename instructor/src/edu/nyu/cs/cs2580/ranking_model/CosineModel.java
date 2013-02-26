ab7a203b6468ef50ba7df4e2223ddc328d0005f0
kunalbalani <kunal.balani@nyu.edu>
2013-02-26 02:52:43 -0500

    adding phrase ranker

---
+++ instructor/src/edu/nyu/cs/cs2580/ranking_model/CosineModel.java
@@ -0,0 +1,24 @@
+package edu.nyu.cs.cs2580.ranking_model;
+
+import java.util.Vector;
+
+public class CosineModel extends Model
+{
+
+	/**
+	 * Accepts document Vector and Query Vector
+	 * @param dv
+	 * @param qv
+	 */
+	public CosineModel(Vector<String> dv,Vector<String> qv)
+	{
+		
+	}
+	
+	@Override
+	public Float getScore() 
+	{
+		return null;
+	}
+
+}
---
+++ instructor/src/edu/nyu/cs/cs2580/ranking_model/QLModel.java
@@ -0,0 +1,24 @@
+package edu.nyu.cs.cs2580.ranking_model;
+
+import java.util.Vector;
+
+public class QLModel extends Model 
+{
+	/**
+	 * Accepts document Vector and Query Vector
+	 * @param dv
+	 * @param qv
+	 */
+	public QLModel(Vector<String> dv,Vector<String> qv)
+	{
+		
+	}
+	
+	@Override
+	public Float getScore() 
+	{
+		return null;
+	}
+
+
+}
