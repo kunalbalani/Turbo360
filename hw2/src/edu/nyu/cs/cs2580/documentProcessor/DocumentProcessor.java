e297a4fc66c95363a74e9a2dc9452c6c14ab923d
Samit <samit.patel@nyu.edu>
2013-03-09 19:48:26 -0500

    Added Document Processor including HTML Parser, PorterStemming and Stopwords

---
+++ hw2/src/edu/nyu/cs/cs2580/documentProcessor/DocumentProcessor.java
@@ -0,0 +1,77 @@
+package edu.nyu.cs.cs2580.documentProcessor;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+import java.util.Vector;
+
+import de.l3s.boilerpipe.BoilerpipeProcessingException;
+import de.l3s.boilerpipe.extractors.ArticleExtractor;
+
+public class DocumentProcessor {
+
+	StemmingAndStopWordsWrapper stemmingAndStopWordsWrapper;
+
+	DocumentProcessor(){
+		stemmingAndStopWordsWrapper 
+		= new StemmingAndStopWordsWrapper(new PorterStemmer());
+	}
+
+	/**
+	 * Process the text to include the following operations (removes the html tags, 
+	 * replace punctuations with whitespace, downcase the words, 
+	 * removes the stopwords and stems the words)
+	 * 
+	 * @param fileReader FileReader object
+	 * */
+	public Vector<String> process(FileReader fileReader) throws BoilerpipeProcessingException{
+
+		Scanner scan = new Scanner(fileReader);  
+		//reads all the text at once
+		scan.useDelimiter("\\Z");  
+		String content = scan.next();  
+
+		return process(content);
+	}
+
+
+	/**
+	 * Process the text to include the following operations (removes the html tags, 
+	 * replace punctuations with whitespace, downcase the words, 
+	 * removes the stopwords and stems the words)
+	 * 
+	 * @param text text to be process
+	 * */
+	public Vector<String> process(String text) throws BoilerpipeProcessingException{
+
+		Vector<String> processedTokens = null;
+
+		try{
+			//removes HTML
+			String content = ArticleExtractor.getInstance().getText(text);
+
+			//removes non-alphanumeric characters
+			content = content.replaceAll("\\W", " ");
+
+			//splits based on whitespace
+			String[] tokens = content.split("\\s+");
+			
+			processedTokens = stemmingAndStopWordsWrapper.process(tokens);
+			
+		}catch(BoilerpipeProcessingException e){
+			e.printStackTrace();
+		}
+
+		return processedTokens;
+	}
+
+
+//	public static void main(String args[]) throws FileNotFoundException, BoilerpipeProcessingException{
+//
+//		FileReader fileReader = new FileReader("data/wiki/(Shake,_Shake,_Shake)_Shake_Your_Booty");
+//
+//		DocumentProcessor dp = new DocumentProcessor();
+//
+//		System.out.println(dp.process(fileReader));
+//	}
+}
